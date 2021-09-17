857bf43286753f46be1cbacb65e358cb6207ac5f
Gene Liverman <gene.liverman+06301990@puppet.com>
2021-09-17 15:24:17 -0400

    1.0.0 release prep

--- manifests/register.pp
+++ manifests/register.pp
@@ -6,6 +6,15 @@
 # you can use `meta_motd::fragment` or `meta_motd::keyvalue.
 #
 # @api private
+#
+# @param [String[1]] content
+#   A string in "key: value" format. The key must start with a word character (regex `\w`).
+#   It can then have zero or more spaces, word characters, or dashes.
+#   Next it must contain another word character followed by a colon and a space.
+#
+# @param [String[2]] order
+#   Orders entries within the motd. Entries that share the same order number
+#   are sorted by name.
 define meta_motd::register (
   String[1] $content = $title,
   String[2] $order = '20',
